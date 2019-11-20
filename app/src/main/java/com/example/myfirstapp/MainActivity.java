fa038e31f89609fac66df2ffb0581e1efc03e501
Jeffrey Chang <thelostpotato27@gmail.com>
2019-11-20 10:47:46 -0800

    moving work area
    Moved over from one computer to another, just moving the most updated version of the code over as well

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.my_first_app.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
     //Called when someone taps the button
     public void sendMessage (View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
+
+//helloo how are you all
 
